6ada67e312d1207396dc46bdd37a4122e037c4ac
Carol <xuanli.carol@gmail.com>
2024-04-18 14:48:39 +0800

    项目初始构建

--- gulimall-product/src/test/java/com/test/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/test/gulimall/product/GulimallProductApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
